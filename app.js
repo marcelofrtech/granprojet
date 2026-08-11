02cc717611bf5b911bec359d43e8a92d6fd25ca9
Marcelo França da Silva <marcelofrancatec@gmail.com>
2026-08-11 16:17:03 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('Olá, Mundo!');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/`);
+});
